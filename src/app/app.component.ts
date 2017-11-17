21d2e8653298375bbf569fb8baafb2438579b6dc
Beom <21300356@handong.edu>
2017-11-17 15:53:18 +0900

    23

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,32 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import firebase  from 'firebase';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDQd7tcWiVBnlSWazxLD24sCj_HFxTu604",
+      authDomain: "midterm-7e42e.firebaseapp.com",
+      databaseURL: "https://midterm-7e42e.firebaseio.com",
+      projectId: "midterm-7e42e",
+      storageBucket: "midterm-7e42e.appspot.com",
+      messagingSenderId: "517113250841"
+    });
+    const unsubscribe = firebase.auth().onAuthStateChanged( user => {
+      if(!user){
+        this.rootPage = 'LoginPage';
+        unsubscribe();
+      } else {
+        this.rootPage = HomePage;
+        unsubscribe();
+      }
+    });  
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
